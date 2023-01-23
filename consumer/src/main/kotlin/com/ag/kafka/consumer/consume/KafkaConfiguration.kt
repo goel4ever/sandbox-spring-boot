88890eea10e6221f7fde07fcb1a1bf6892e7c2b0
Anshul Goel <dec.anshul@gmail.com>
2023-01-23 12:26:53 -0500

    added batch consumer configs

--- consumer/src/main/kotlin/com/ag/kafka/consumer/consume/KafkaConfiguration.kt
+++ consumer/src/main/kotlin/com/ag/kafka/consumer/consume/KafkaConfiguration.kt
@@ -1,6 +1,7 @@
 package com.ag.kafka.consumer.consume
 
 import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.annotation.EnableKafka
@@ -14,6 +15,14 @@ import org.springframework.kafka.listener.ConcurrentMessageListenerContainer
 @Configuration
 @EnableKafka
 class KafkaConfiguration {
+
+    @Value("\${spring.kafka.bootstrapServers}") private lateinit var bootstrapServersConfig: String
+    @Value("\${spring.kafka.consumer.group-id}") private lateinit var groupId: String
+    @Value("\${spring.kafka.consumer.keyDeserializer}") private lateinit var keyDeserializer: String
+    @Value("\${spring.kafka.consumer.valueDeserializer}") private lateinit var valueDeserializer: String
+    @Value("\${spring.kafka.consumer.max-poll-records}") private lateinit var maxPollRecords: String
+    @Value("\${spring.kafka.consumer.auto-offset-reset}") private lateinit var autoOffsetReset: String
+
     @Bean
     fun kafkaListenerContainerFactory(): KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<Int?, String?>>? {
         val factory = ConcurrentKafkaListenerContainerFactory<Int, String>()
@@ -32,10 +41,11 @@ class KafkaConfiguration {
     @Bean
     fun consumerConfigs(): Map<String, String> {
         val props: HashMap<String, String> = HashMap()
-        props[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = "localhost:9092"
-        props[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = "org.apache.kafka.common.serialization.StringDeserializer"
-        props[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = "org.apache.kafka.common.serialization.StringDeserializer"
-        props[ConsumerConfig.MAX_POLL_RECORDS_CONFIG] = "5"
+        props[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapServersConfig
+        props[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = keyDeserializer
+        props[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = valueDeserializer
+        props[ConsumerConfig.MAX_POLL_RECORDS_CONFIG] = maxPollRecords
+        props[ConsumerConfig.AUTO_OFFSET_RESET_CONFIG] = autoOffsetReset
         return props;
     }
 }
